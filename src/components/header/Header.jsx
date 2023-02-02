512cac060789bda3af695ca983d9ac880a33f8e9
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-02-02 13:13:19 -0600

    added navigation to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './header.css';
+import { HiOutlineHome, HiOutlineLightningBolt, HiX, HiOutlineMenu } from 'react-icons/hi';
+
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    window.addEventListener("scroll", function () {
+        const header = document.querySelector(".header");
+        if(this.scrollY >= 80) header.classList.add("scroll-header");
+        else header.classList.remove("scroll-header");
+    });
+   
+    const[Toggle, showMenu] = useState(false);
+    const[activeNav, setActiveNav] = useState("#home");
+
+    return(
+        <header className="header">
+            <nav className="nav container">
+                <a href="index.html" className="nav__logo">Anime[In'yō]</a>
+                <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+                    <ul className="nav__list grid">
+                        <li className="nav__item">
+                            <a href="#home" onClick={() => setActiveNav("#home")} 
+                            className={activeNav === "#home" ? "nav__link active-link" : "nav__link"}>
+                                <HiOutlineHome className="nav__icon"/>Home
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#generator" onClick={() => setActiveNav("#generator")} 
+                            className={activeNav === "#generator" ? "nav__link active-link" : "nav__link"}>
+                                <HiOutlineLightningBolt className="nav__icon"/>Generator
+                            </a>
+                        </li>
+                    </ul>
+                    <HiX className="nav__close" onClick={() => showMenu(!Toggle)} />
+                </div>
+                <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+                    <HiOutlineMenu />
+                </div>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
