91d727f1354d8cddbf41d47c21d94cb69d87ba2b
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-02-02 14:04:14 -0600

    set home layout and placeholders

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Data = () => {
+  return (
+    <div className="home__data">
+        <h1 className="home__title">Anime[In'yō]</h1>
+        <h3 className="home__subtitle">Anime quotes at your fingertips</h3>
+        <p className="home__description">Generate quotes and facts from your favorite Anime character or at random.</p>    
+        <a href="#generator" className="button button--flex">Let's Go!</a>
+    </div>
+  );
+}
+
+export default Data;
