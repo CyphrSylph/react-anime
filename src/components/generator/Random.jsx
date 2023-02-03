67a9d63e04032c3200efab2fd05b691bc873d4aa
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-02-03 17:33:35 -0600

    added section for random data in quote generator

--- src/components/generator/Random.jsx
+++ src/components/generator/Random.jsx
@@ -3,7 +3,15 @@ import './random.css';
 
 const Random = () => {
   return (
-    <div>Random</div>
+	<div className="random__container container grid">
+		<div className="random__content grid">
+			<div className="anime__img"></div>
+			<div className="random__data">
+				<h3 className="random__quote">Quote</h3>
+				<p className="random__details">Character Name, Anime Title</p>
+			</div>
+		</div>
+	</div> 
   );
 }
 
