8629518b258e7a215e2ab9d3163f5cccf3a564db
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-02-04 21:30:44 -0600

    updated grid template and positioning

--- src/components/generator/Random.jsx
+++ src/components/generator/Random.jsx
@@ -5,7 +5,7 @@ const Random = () => {
   return (
 	<div className="random__container container grid">
 		<div className="random__content grid">
-			<div className="anime__img"></div>
+			<div className="random__img"></div>
 			<div className="random__data">
 				<h3 className="random__quote">Quote</h3>
 				<p className="random__details">Character Name, Anime Title</p>
